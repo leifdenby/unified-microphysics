18d502db640974118bf0e6372bd3c419102a28eb
Leif Denby <leif@denby.eu>
2015-10-20 12:45:30 +0200

    First step of clean, new folder structure

--- exception_test.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-import test
-test.microphysics_pylib.calc_dq(0., 0., 0., 0., 0.,)
-test.microphysics_pylib.raise_python_exception(0)
--- test_no_ice.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-import unified_microphysics
-unified_microphysics.microphysics_pylib.calc_dq([1.,0.], [[1.,],], dt=0.1, temp=300.15, pressure=101325.0)
-
-unified_microphysics.microphysics_pylib.init('no_ice')
-unified_microphysics.microphysics_pylib.calc_dq([1.,0.], [[1.,],], dt=0.1, temp=300.15, pressure=101325.0)
