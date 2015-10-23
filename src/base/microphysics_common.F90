aa94c5d9dd882bb57d4745c3bdc38ad672cb3465
Leif Denby <leif@denby.eu>
2015-10-23 12:31:48 +0200

    Move Teten's formula constants

--- src/base/microphysics_common.F90
+++ src/base/microphysics_common.F90
@@ -29,21 +29,34 @@ module microphysics_common
       end function water_vapour_diffusivity
 
       function saturation_vapour_pressure(temp)
-         use microphysics_constants, only: epsmach, T0
+         use microphysics_constants, only: epsmach, T0, p0vs, a0_lq, a1_lq, a0_ice, a1_ice
 
          real(kreal) :: saturation_vapour_pressure
          real(kreal), intent(in) :: temp
 
-         real(kreal), parameter :: awat=17.25
-         real(kreal), parameter :: bwat=36.
-         real(kreal), parameter :: asat=610.7
-
          real(kreal) :: expon2, satpw
 
-         expon2=awat*(temp-t0)/max(temp-bwat,epsmach)
-         saturation_vapour_pressure=asat*exp(expon2)
-
+         if (temp > T0) then
+             expon2=a0_lq*(temp-t0)/max(temp+a1_lq,epsmach)
+         else
+             expon2=a0_ice*(temp-t0)/max(temp+a1_ice,epsmach)
+         endif
+         saturation_vapour_pressure=p0vs*exp(expon2)
       end function saturation_vapour_pressure
+
+      function saturation_vapour_concentration(temp, p)
+         use microphysics_constants, only: epsmach, T0, R_v, R_d
+
+         real(kreal), intent(in) :: temp, p
+         real(kreal) :: saturation_vapour_concentration
+
+         real(kreal) :: pv_sat, eps
+
+         pv_sat = saturation_vapour_pressure(temp)
+         eps = R_d/R_v
+         saturation_vapour_concentration = (eps*pv_sat)/(p-(1.-eps)*pv_sat)
+      end function saturation_vapour_concentration
+
 
       function cv_mixture(q_gases, q_solids)
          use microphysics_register, only: n_gases, n_solids, n_moments__max, cv_solids, cv_gases
